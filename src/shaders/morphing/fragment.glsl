b8315ae02c5a8a2cc3b09dcfc9d94a34b10b6dae
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-02-24 13:01:24 +0100

    Modify particle dot to star

--- src/shaders/morphing/fragment.glsl
+++ src/shaders/morphing/fragment.glsl
@@ -5,7 +5,10 @@ varying vec3 vColor;
 
 void main(){
   vec2 uv = gl_PointCoord;
-  float aplha = 0.05 /  length(uv-0.5) - 0.1;
+  float horizontalElipse=.5/(distance(vec2(uv.x,(uv.y-.5)*10.+.5),vec2(.5)));
+  float verticalEllipse=.5/(distance(vec2((uv.x-.5)*10.+.5,uv.y),vec2(.5)));
+
+  float aplha = verticalEllipse*horizontalElipse;
   gl_FragColor=vec4(vColor, aplha);
 
   #include <tonemapping_fragment>
