f2fcd37d8dc2d277ca07dbc4d8e6ce11c93f4955
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-05-29 23:45:46 +0200

    Add leva controls

--- src/pages/lab/imageShader/PlaneImage.tsx
+++ src/pages/lab/imageShader/PlaneImage.tsx
@@ -1,17 +1,48 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 import vertex from "@shaders/imageShader/vertex.glsl";
 import fragment from "@shaders/imageShader/fragment.glsl";
-import { useFrame } from "@react-three/fiber";
+import { useFrame, useThree } from "@react-three/fiber";
 import { useEffect, useMemo } from "react";
 import { Uniform, Vector2 } from "three";
 import { useTexture } from "@react-three/drei";
+import { useControls } from "leva";
 
 export const PlaneImage = () => {
   const tower = useTexture("/tower.png");
-  const planeArgs = new Vector2(2, 10);
+  const { viewport } = useThree();
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [arg, _set] = useControls(() => ({
+    width: {
+      value: 5,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+    height: {
+      value: 3,
+      min: 1,
+      max: 10,
+      step: 1,
+    },
+    progress: {
+      value: 0,
+      min: 0,
+      max: 1,
+      step: 0.01,
+    },
+  }));
+
+  const planeArgs = new Vector2(arg.width, arg.height);
+
+  useEffect(() => {
+    uniforms.uPlaneAspect.value = planeArgs;
+    uniforms.uProgress.value = arg.progress;
+  }, [arg]);
 
   const uniforms = useMemo(
     () => ({
+      uViewport: new Uniform(new Vector2(viewport.width, viewport.height)),
       uImageResolution: new Uniform(new Vector2(1, 1)),
       uPlaneAspect: new Uniform(planeArgs),
       uTime: new Uniform(0),
--- src/shaders/imageShader/vertex.glsl
+++ src/shaders/imageShader/vertex.glsl
@@ -1,11 +1,18 @@
 varying vec2 vUv;
+uniform float uProgress;
+uniform vec2 uPlaneAspect;
+uniform vec2 uViewport;
+
 
 void main(){
-  vec4 modelPosition = modelMatrix * vec4(position, 1.);
+  vec3 newPosition = position;
+  newPosition.xy /= uPlaneAspect;
+  vec3 finalPosition = mix(position,newPosition, uProgress);
+  vec4 modelPosition = modelMatrix * vec4(finalPosition, 1.);
   vec4 viewPosition = viewMatrix * modelPosition;
   vec4 projectedPosition = projectionMatrix * viewPosition;
 
   gl_Position = projectedPosition;
 
-  vUv = uv;
+  vUv = mix(uv,uv / uPlaneAspect, uProgress);
 }
