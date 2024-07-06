ed6d1571bc5cf0fb34d9e4fec958858d0b0b3c32
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-07-06 19:05:15 +0200

    :refactor: change data json in ts file

---
+++ src/data/projects.ts
@@ -0,0 +1,38 @@
+export const data = {
+  lab: {
+    name: "Lab experiments",
+    baseRoute: "lab",
+    data: [
+      {
+        name: "Precedural Terrain",
+        route: "procedural-terrain",
+      },
+      {
+        name: "Grass",
+        route: "grass",
+      },
+      {
+        name: "Image shader",
+        route: "image-shader",
+      },
+      {
+        name: "Particles morphing",
+        route: "particles-morphing",
+      },
+    ],
+  },
+  projects: {
+    name: "Projects",
+    baseRoute: "project",
+    data: [
+      {
+        name: "UV Viewer",
+        route: "uv-viewer",
+      },
+      {
+        name: "Futuristic UI",
+        route: "futuristic-ui",
+      },
+    ],
+  },
+};
