53d2d6116dbe2bcbc51b7f7a7a353b142e2d5a03
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-07-08 23:12:42 +0200

    :sparkles: add crystal in scene + envmap

--- src/pages/projects/3-magic-summoning-circles/MagicSummoningCircles.tsx
+++ src/pages/projects/3-magic-summoning-circles/MagicSummoningCircles.tsx
@@ -1,13 +1,16 @@
-import { MapControls } from "@react-three/drei";
+import { Environment, MapControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 import { MagicCircle } from "./components/MagicCircle";
 import { Effects } from "./components/Effects";
+import { Crystal } from "./components/Crystal";
 
 export const MagicSummoningCircles = () => {
   return (
     <Canvas>
       <Effects />
+      <Crystal />
       <MagicCircle />
+      <Environment preset="studio" />
       <MapControls object-position={[0, 5, 5]} />
     </Canvas>
   );
