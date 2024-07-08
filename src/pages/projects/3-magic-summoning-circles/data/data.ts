ba21f578c804f872df7ebc6a2c4c2a95e1bce51f
Cheung-KaWai <84151954+Cheung-KaWai@users.noreply.github.com>
2024-07-08 23:13:05 +0200

    :sparkles: add data structure for logic game

---
+++ src/pages/projects/3-magic-summoning-circles/data/data.ts
@@ -0,0 +1,32 @@
+export const combinations = [
+  {
+    id: "water",
+  },
+  {
+    id: "fire",
+  },
+  {
+    id: "earth",
+  },
+];
+
+export const dataCircles = {
+  outerCircle: [
+    {
+      id: "water",
+      path: "/3-magic-summoning-circles/circles/water_outer.png",
+    },
+  ],
+  innerCircle: [
+    {
+      id: "water",
+      path: "/3-magic-summoning-circles/circles/water_inner.png",
+    },
+  ],
+  centerCircle: [
+    {
+      id: "water",
+      path: "/3-magic-summoning-circles/circles/water_center.png",
+    },
+  ],
+};
